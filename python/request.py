3ddadff6acf8be9211d5852075cedc548a2f150e
Clemens <clemens.koehler@mail.de>
2023-02-14 13:58:48 +0100

    geändert:       python/request.py

--- python/request.py
+++ python/request.py
@@ -1,9 +1,15 @@
-import request
+import requests
 
 ## now you can just start to defind the url
 
 url = "https://httpbin.org/get"
 
-r = request.get(url)
+r = requests.get(url)
 
-r.status_code ## this showes the code for this request
+r.status_code ## this showes the code for this request 
+
+## 200 => Success, 400 => Bad request,500 => Internal server error, 404 => Not found,
+#  401 => Unauthorized
+
+content_type = r.headers['content-type'] ## This line returns the content type from the answer
+## e.g. 'application/json; charset=utf8' means JSON as answer
