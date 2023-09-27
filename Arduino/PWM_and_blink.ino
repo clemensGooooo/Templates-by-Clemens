313c547f173734253598e2f0ec5b9a0767e7b12e
Clemens <100614200+clemensGooooo@users.noreply.github.com>
2023-09-27 21:02:29 +0200

    Create PWM_and_blink.ino

---
+++ Arduino/PWM_and_blink.ino
@@ -0,0 +1,24 @@
+int sensorValue = 0;
+int led = 10; // only PWM pins
+int mode = 2;  // mode one will change the blinking rate of the led, mode two will change the brightness
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  sensorValue = analogRead(A0);
+  if (mode == 1) {
+    delay(sensorValue / 4);
+    digitalWrite(led, HIGH);
+
+    delay(sensorValue / 4);
+    digitalWrite(led, LOW);
+  } else {
+    analogWrite(led, sensorValue / 4);
+  }
+  // Serial.println(sensorValue);
+}
